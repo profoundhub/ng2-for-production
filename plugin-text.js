ce32b98ad08146b069eaea82f87e3d4b6beb7dfd
Daniel Lim <hbt@markhamboards.com>
2016-10-23 17:50:22 -0400

    update compile

--- plugin-text.js
+++ plugin-text.js
@@ -1,9 +1,9 @@
 exports.translate = function(load) {
   if (this.builder && this.transpiler) {
     load.metadata.format = 'esm';
-    return 'export var __useDefault = true; export default ' + JSON.stringify(load.source) + ';';
+    return 'exp' + 'ort var __useDefault = true; exp' + 'ort default ' + JSON.stringify(load.source) + ';';
   }
 
   load.metadata.format = 'amd';
-  return 'define(function() {\nreturn ' + JSON.stringify(load.source) + ';\n});';
+  return 'def' + 'ine(function() {\nreturn ' + JSON.stringify(load.source) + ';\n});';
 }
